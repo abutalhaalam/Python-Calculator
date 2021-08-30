a1252a109d257e8e4d53c0cbacb11e8970ebd39d
AA <86781896+abutalhaalam@users.noreply.github.com>
2021-08-30 11:26:15 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -6,14 +6,17 @@ def multiply(x,y):
     return x * y
 def divide(x,y):
     return x / y
+def exponents(x,y):
+    return x ** y
 while True:
     print('Basic Calculator\nSelect your Operator')
     print('1. Add')
     print('2. Subtract')
-    print('3. multiply')
+    print('3. Multiply')
     print('4. Divide')
-    Operator = input("Select your input(1/2/3/4): ")
-    if Operator in ('1','2','3','4'):
+    print('5. Exponents')
+    Operator = input("Select your input(1/2/3/4/5): ")
+    if Operator in ('1','2','3','4', '5'):
         try:
             num1 = float(input("Enter first Number: "))
             num2= float(input("Enter Second Number: "))
@@ -41,8 +44,14 @@ while True:
                 num1 = str(num1)
                 num2 = str(num2)
                 print(num1+'/'+num2 +'='+ans)
+            if Operator in ('5'):
+                ans = exponents(num1, num2)
+                ans = str(ans)
+                num1 = str(num1)
+                num2 = str(num2)
+                print(num1 + '^' + num2 + '=' + ans)
         except:
             print('Invalid Number')
-        print('Next Challenge \n ')
+        print('Next Challenge\n\ngithub.com/abutalhaalam\n\n ') #github.com/abutalhaalam
     else:
         print('\nInvalid Operator\nTry Again\n ')
